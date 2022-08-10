175a103468ef7afb7fe4ba0025e33f34cc346f86
birkan <b.561birkan@gmail.com>
2022-08-10 18:25:33 +0300

    working on Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,30 @@
-const Search = () => {
+import { useState } from "react";
+
+const Search = ({ setSearch }) => {
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch(name);
+    // console.log(name);
+    setName("");
+  };
   return (
-    <div>
-      <h1>search</h1>
-    </div>
+    <form className="input-group m-5 text-center w-25" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter Country Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        className="btn btn-outline-secondary bg-danger text-white"
+        type="button"
+        id="button-addon2"
+      >
+        Search
+      </button>
+    </form>
   );
 };
 export default Search;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import Main from "../components/Main";
 import Search from "../components/Search";
 
 const Home = () => {
+  const [search, setSearch] = useState();
   return (
     <div>
-      <Search />
-      <Main />
+      <Search setSearch={setSearch} />
+      <Main search={search} />
     </div>
   );
 };
