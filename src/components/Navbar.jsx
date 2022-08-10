2e77e2ff8c8eef02ed76f028b41d7a922b4623d2
birkan <b.561birkan@gmail.com>
2022-08-10 23:59:22 +0300

    Navbar component is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a
+          className="navbar-brand"
+          href="https://github.com/birkan-dogan/rest-countries"
+          target="_blank"
+        >
+          <img
+            src="https://avatars.githubusercontent.com/u/101419153?v=4"
+            alt=""
+            width={30}
+            height={30}
+            className="d-inline-block align-text-top nav-img"
+          />
+          rest countries
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
